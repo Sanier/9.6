db31a381dbbefca0b3fa60d147a860d1dd920d96
Sanier <Sanier20@mail.ru>
2021-10-13 19:37:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _9._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
